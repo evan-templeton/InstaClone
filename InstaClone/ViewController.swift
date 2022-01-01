7bf32a9d8843f9eb827d0dc24fd49ea2e1140f9b
Evan Templeton <temevan@amazon.com>
2022-01-01 09:36:06 -0600

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Templeton, Evan on 1/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
